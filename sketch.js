085b13b3d561cf18b850efcd82d62119646746fe
anukriti1206 <71882793+anukriti1206@users.noreply.github.com>
2020-11-24 18:55:41 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,56 @@
+var dog, dogImg, happydog, database, foodS, foodStock;
+
+function preload()
+{
+  dogImg = loadImage("images/dogImg.png");
+  happydog = loadImage("images/dogImg1.png");
+
+  
+}
+
+function setup() {
+	createCanvas(500,500);
+  
+  database = firebase.database();
+  dog = createSprite(230,250, 50,50);
+  dog.addImage(dogImg);
+  dog.scale = 0.2;
+
+  foodStock = database.ref('food');
+  foodStock.on("value", readStock);
+}
+
+
+function draw() {  
+
+  background(46,139,87);
+  if(keyWentDown("UP_ARROW")){
+    writeStock(foodS);
+    dog.addImage(happydog);
+    dog.scale = 0.2;
+  }
+  drawSprites();
+  
+  fill('white');
+  text("Food Stock : " + foodS, 400, 20);
+  text("Note : Press the UP_ARROW key to feed the dog milk!", 130, 470);
+
+}
+
+function readStock(data){
+   foodS = data.val();
+}
+
+function writeStock(x){
+
+  if(x <= 0){
+    x = 0;
+  }
+  else{
+    x = x - 1;
+  }
+   database.ref('/').update({
+     food : x
+   })
+}
+
